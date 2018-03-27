c000c6ce8a6526068d6f87893a3a97faf2c53196
July <Luxiaowen1234>
2018-03-27 14:21:48 +0800

    添加log

---
+++ src/test/java/cn/luxw/app/jdk8/doc/package-info.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+/**
+ * 1.lambda表达式
+ * @FilterApple @LambdaExpression @MethodReference
+ * 2.stream
+ */
+package cn.luxw.app.jdk8.doc;
+
+
+/*
+//1. 创建 Stream
+	@Test
+	public void test1(){
+		//1. Collection 提供了两个方法  stream() 与 parallelStream()
+		List<String> list = new ArrayList<>();
+		Stream<String> stream = list.stream(); //获取一个顺序流
+		Stream<String> parallelStream = list.parallelStream(); //获取一个并行流
+		
+		//2. 通过 Arrays 中的 stream() 获取一个数组流
+		Integer[] nums = new Integer[10];
+		Stream<Integer> stream1 = Arrays.stream(nums);
+		
+		//3. 通过 Stream 类中静态方法 of()
+		Stream<Integer> stream2 = Stream.of(1,2,3,4,5,6);
+		
+		//4. 创建无限流
+		//迭代
+		Stream<Integer> stream3 = Stream.iterate(0, (x) -> x + 2).limit(10);
+		stream3.forEach(System.out::println);
+		
+		//生成
+		Stream<Double> stream4 = Stream.generate(Math::random).limit(2);
+		stream4.forEach(System.out::println);
+		
+	}*/
--- src/test/java/cn/luxw/app/jdk8/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * 1.lambda表达式
- * @FilterApple @LambdaExpression @MethodReference
- * 2.stream
- */
-package cn.luxw.app.jdk8;
